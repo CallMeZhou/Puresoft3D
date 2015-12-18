31c9d523952d590846692e1e2a3cb26c6d588364
agedboy <agedboy@sina.com>
2015-12-18 17:33:27 +0800

    Add multi-range function to VBO class

--- src/puresoft3d/vbo.cpp
+++ src/puresoft3d/vbo.cpp
@@ -7,15 +7,15 @@ using namespace std;
 PuresoftVBO::PuresoftVBO(size_t unitBytes, size_t unitCount)
 {
 	m_unitBytes = unitBytes;
-	m_bufferBytes = m_unitBytes * unitCount;
+	m_unitCount = unitCount;
+	m_bufferBytes = unitBytes * unitCount;
 
 	if(NULL == (m_buffer = _aligned_malloc(m_bufferBytes, 16)))
 	{
 		throw bad_alloc("PuresoftVBO::updateContent");
 	}
 
-	m_currentPosition = (unsigned char*)m_buffer;
-	m_endPosition = m_currentPosition + m_bufferBytes;
+	rewindRanges(-1);
 }
 
 
@@ -29,20 +29,62 @@ void PuresoftVBO::updateContent(const void* src)
 	memcpy(m_buffer, src, m_bufferBytes);
 }
 
-void PuresoftVBO::rewind(void)
+void PuresoftVBO::rewindRanges(int idx /* = -1 */)
 {
-	m_currentPosition = (unsigned char*)m_buffer;
+	if(idx < 0)
+	{
+		for(size_t i = 0; i < MAX_WORKRANGES; i++)
+		{
+			m_workRanges[i].current = (unsigned char*)m_buffer;
+			m_workRanges[i].end = (unsigned char*)m_buffer + m_bufferBytes;
+		}
+	}
+	else if(idx >= MAX_WORKRANGES)
+	{
+		throw out_of_range("PuresoftVBO::rewindRanges");
+	}
+	else
+	{
+		m_workRanges[idx].current = (unsigned char*)m_buffer;
+		m_workRanges[idx].end = (unsigned char*)m_buffer + m_bufferBytes;
+	}
 }
 
-const void* PuresoftVBO::next(void)
+void PuresoftVBO::evenOutRanges(int rangeAmount /* = MAX_WORKRANGES */)
 {
-	if(m_currentPosition >= m_endPosition)
+	if(rangeAmount <= 0 && rangeAmount > MAX_WORKRANGES)
+	{
+		throw invalid_argument("PuresoftVBO::evenOutRanges");
+	}
+
+	size_t unitsPerRange = m_unitCount / rangeAmount;
+	size_t bytesPerRange = unitsPerRange * m_unitBytes;
+
+	m_workRanges[0].current = (unsigned char*)m_buffer;
+	m_workRanges[0].end = (unsigned char*)m_buffer + bytesPerRange;
+	for(int i = 1; i < rangeAmount; i++)
+	{
+		m_workRanges[i].current = m_workRanges[i - 1].end;
+		m_workRanges[i].end = m_workRanges[i].current + bytesPerRange;
+	}
+}
+
+const void* PuresoftVBO::next(size_t idx /* = 0 */)
+{
+	if(idx < 0 || idx >= MAX_WORKRANGES)
+	{
+		throw out_of_range("PuresoftVBO::next");
+	}
+
+	WORKRANGE& range = m_workRanges[idx];
+
+	if(range.current >= range.end)
 	{
 		return NULL;
 	}
 
-	const void* ret = m_currentPosition;
-	m_currentPosition += m_unitBytes;
+	const void* ret = range.current;
+	range.current += m_unitBytes;
 
 	return ret;
 }
--- src/puresoft3d/vbo.h
+++ src/puresoft3d/vbo.h
@@ -3,17 +3,28 @@
 class PuresoftVBO
 {
 public:
+	__declspec(align(64)) struct WORKRANGE
+	{
+		unsigned char* current;
+		unsigned char* end;
+	};
+
+	static const size_t MAX_WORKRANGES = 16;
+
+public:
 	PuresoftVBO(size_t unitBytes, size_t unitCount);
 	~PuresoftVBO(void);
 	void updateContent(const void* src);
-	void rewind(void);
-	const void* next(void);
+	void rewindRanges(int idx = -1); // -1 means rewind all
+	void evenOutRanges(int rangeAmount = MAX_WORKRANGES);
+	const void* next(size_t idx = 0);
 
 private:
 	size_t m_unitBytes;
+	size_t m_unitCount;
 	size_t m_bufferBytes;
 	void* m_buffer;
-	unsigned char* m_currentPosition;
-	unsigned char* m_endPosition;
+
+	WORKRANGE m_workRanges[MAX_WORKRANGES];
 };
 
